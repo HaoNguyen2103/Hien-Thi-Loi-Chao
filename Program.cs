574b6c8a6e27752142d0bd50c86df4d6714dc3ea
HaoNguyen2103 <nguyenhao21031995@gmail.com>
2025-08-01 21:53:04 +0700

    Ten
    2

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap ten cua ban: ");
+       string Ten = Console.ReadLine();
+        Console.WriteLine("Hello " + Ten);
+    }
+}
